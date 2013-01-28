d5c9c20affa7b289667c4d8b8a577a6c95c38054
websanova <rob@websanova.com>
2013-01-28 14:20:41 -0500

    First release

---
+++ README.md
@@ -0,0 +1,178 @@
+# wTooltip.js
+
+A jQuery modal plugin. [Check out the live demo](http://www.websanova.com/plugins/modals).
+
+
+ - [Settings](https://github.com/websanova/wModal#settings)
+ - [Examples](https://github.com/websanova/wModal#examples)
+ - [Adding Messages](https://github.com/websanova/wModal#addingmessages)
+ - [Adding Modals](https://github.com/websanova/wModal#addingmodals)
+ - [Adding Buttons](https://github.com/websanova/wModal#addingbuttons)
+ - [Adding Effects](https://github.com/websanova/wModal#addingeffects)
+
+
+## Settings
+
+Available options with notes, the values here are the defaults.
+
+```
+$('#_wModal_yesNo').wTooltip({
+    position    : 'cm',     // position of modal (lt, ct, rt, rm, rb, cb, lb, lm, cm)
+    offset      : '10',       // offset of modal from edges if not in "m" or "c" position
+    fxShow      : 'none',   // show effects (fade, slideUp, slideDown, slideLeft, slideRight)
+    fxHide      : 'none',   // hide effects (fade, slideUp, slideDown, slideLeft, slideRight)
+    btns        : {},       // button callbacks
+    msg         : null      // optional message to set if "_wModal_msg" class is set
+});
+```
+
+
+## Examples
+
+Modals can be any piece of html and you can create your own or build one using something like Twitter Boostrap.  You can stylize to your hearts content or use some of the defaults provided in wModal.
+
+```
+<script type="text/template" id="wModal_yesNo">
+    <div class="_wModal _wModal_yesNo">
+        <div class="_wModal_header">
+            Websanova
+            <div class="_wModal_close _wModal_btn_cancel">x</div>
+        </div>
+
+        <div class="_wModal_msg">Are you sure you want to do that?</div>
+        
+        <div class="_wModal_btns_right">
+            <div class="_wModal_btn _wModal_btn_grey _wModal_btn_no">No</div>
+            <div class="_wModal_btn _wModal_btn_blue _wModal_btn_yes">Yes</div>
+        </div>
+    </div>
+</script>
+```
+
+
+We can then initialize any modal like so:
+
+```
+$('#wModal_yesNo').wModal({
+    position:'lt',
+    btns:{
+        yes: function()
+        {
+            console.log('yes left top');
+            this.hide();
+        }
+    }
+});
+```
+
+We would then call the modal:
+
+```
+$('#wModal_yesNo').wModal('show');
+```
+
+Any of the inital values can be overwritten temporarily for any other call:
+
+```
+$('#wModal_yesNo').wModal('show', {
+    offset:'50',
+    fxShow:'slideUp',
+    fxHide:'slideDown',
+    btns:{
+        yes: function()
+        {
+            console.log('this is temporary');
+            this.hide();
+        }
+    }
+});
+```
+
+Calling the original modal without options will again display the modal with the initial settings:
+
+```
+$('#wModal_yesNo').wModal('show');
+```
+
+
+## Adding Messages
+
+There is an optional key class that can be set named `_wModal_msg`, which if set will allow you to set messages on the fly. Using the example above we could then set the `msg` setting to any string and it will appear in the target `wModal_msg` area.
+
+```
+$('#wModal_yesNo').wModal('show', {msg:"This is a temporary message."});
+```
+
+Note that if the class `_wModal_msg` is not set then the `msg` option will not do anything and you will need to code your own messages into your prompts.
+
+
+## Adding Modals
+
+The plugin can work off any piece of `html`, with the only requirement being that the modal should have some set width.  You can then use any of the show/hide effects available to the plugin.
+
+Chances are, however that you will want to add some handlers to your button prompts.  You can this by following the [Adding Buttons](https://github.com/websanova/wModal#addingbuttons) section below.
+
+I would recommend using a separate templates file to keep all your prompts nicely organized in, making it easier to modify existing ones and add new ones to the mix.
+
+
+## Adding Buttons
+
+You can set as many buttons as you like using the `btns` option.
+
+```
+$('#wModal_yesNo').wModal('show', {
+    btns:{
+        yes: function(){
+            // code
+            this.hide();
+        },
+        no: function(){
+            //code
+            this.hide();
+        },
+        cancel: function(){
+            //code
+            this.hide
+        }
+        // etc...
+    }
+});
+```
+
+Button handlers are attached based on a naming convention in order to give you flexibility and reusability.  The `wModal` plugin will look for any class names matching `_wModal_btn_<button name>` where  button name is the object key set in the `buttons` parameter.
+
+In the case above `wModal` would look for any elements with class names: `_wModal_btn_yes`, `_wModal_btn_no`, and `_wModal_btn_cancel`.
+
+
+## Adding Effects
+
+The plugin comes packaged with many effects (check options for a full list), however it's easy to add your own.  The effects are called on two key words, `fxShow` and `fxHide`, take a look at the fade effects:
+
+```
+fxShowFade: function()
+{
+    var _this = this;
+    this.bg.fadeIn(100, function(){ _this.modal.fadeIn(300); });
+},
+
+fxHideFade: function()
+{
+    var _this = this;
+    this.modal.fadeOut(300, function(){ _this.bg.fadeOut(100); });
+}
+```
+
+We can then set the `fxShow` and `fxHide` options to `fade`.  You can add as many of your own effects as you like just make sure to show the `bg` and `modal` elements, then hide the `modal` and `bg` elements in those orders for best aesthetic visuals.
+
+
+## Resources
+
+* [jQuery Plugin Development Boilerplate](http://www.websanova.com/tutorials/jquery/jquery-plugin-development-boilerplate)
+* [The Ultimate Guide to Writing jQuery Plugins](http://www.websanova.com/tutorials/jquery/the-ultimate-guide-to-writing-jquery-plugins)
+
+
+## License
+
+MIT licensed
+
+Copyright (C) 2011-2012 Websanova http://www.websanova.com
---
+++ wModal.js
@@ -0,0 +1,281 @@
+/******************************************
+ * Websanova.com
+ *
+ * Resources for web entrepreneurs
+ *
+ * @author          Websanova
+ * @copyright       Copyright (c) 2012 Websanova.
+ * @license         This websanova jQuery modal plugin is dual licensed under the MIT and GPL licenses.
+ * @link            http://www.websanova.com
+ * @github          http://github.com/websanova/modal
+ * @version			1.0.0
+ *
+ ******************************************/
+
+(function($)
+{
+	$.fn.wModal = function(option, settings)
+	{
+		if(typeof option === 'object')
+		{
+			settings = option;
+		}
+		else if(typeof option === 'string')
+		{
+			var values = [];
+
+			var elements = this.each(function()
+			{
+				var data = $(this).data('_wModal');
+
+				if(data)
+				{
+					if(option === 'show') data.show(settings || {});
+					else if(option === 'hide') data.hide(settings || {});
+					else if($.fn.wModal.defaultSettings[option] !== undefined)
+					{
+						if(settings !== undefined) { data.settings[option] = settings; }
+						else { values.push(data.settings[option]); }
+					}
+				}
+			});
+
+			if(values.length === 1) { return values[0]; }
+			else if(values.length > 0) { return values; }
+			else { return elements; }
+		}
+
+		return this.each(function()
+		{
+			var _settings = $.extend({}, $.fn.wModal.defaultSettings, settings || {});
+			var modal = new Modal(_settings, $(this));
+			var $el = modal.generate();
+
+			modal.pixel.append($el);
+
+			$(this).data('_wModal', modal);
+		});
+	}
+
+	$.fn.wModal.defaultSettings = {
+		position	: 'cm',		// position of modal (lt, ct, rt, rm, rb, cb, lb, lm, cm)
+		offset		: '10',		// offset of modal from edges if not in "m" or "c" position
+		fxShow		: 'none',	// show effects (fade, slideUp, slideDown, slideLeft, slideRight)
+		fxHide		: 'none',	// hide effects (fade, slideUp, slideDown, slideLeft, slideRight)
+		btns		: {},		// button callbacks
+		msg			: null		// optional message to set if "_wModal_msg" class is set
+	};
+
+	function Modal(settings, elem)
+	{
+		this.modal = null;
+		this.settings = settings;
+		this.elem = elem;
+
+		this.tempButtons = {};
+
+		return this;
+	}
+
+	Modal.prototype = 
+	{
+		generate: function()
+		{
+			var _this = this;
+
+			if(this.modal) return this.modal;
+
+			// bg - check if bg already exists
+			if($('#_wModal_bg').length)
+			{
+				this.bg = $('#_wModal_bg');
+				this.pixel = $('#_wModal_pixel');
+			}
+			else
+			{
+				this.bg = $('<div id="_wModal_bg"></div>').css({position:'fixed', left:'0', top:'0', display:'none'});
+				$('body').append(this.bg);
+				$(window).resize(function(){ if(_this.bg.is(':visible')) _this.resetBg.apply(_this); });
+
+				// positioning pixel setting to body produces some weird effects wtih scrollbars when doing sliding effects
+				this.pixel = $('<div id="_wModal_pixel"></div>').css({position:'fixed', left:'0', top:'0', width:'0', height:'0', lineHeight:'0', fontSize:'0'});
+				$('body').append(this.pixel);
+			}
+
+			// modal
+			this.modal = $('<div class="_wModal_holder"></div>').css({position:'absolute', display:'none'});
+			this.modal.html(this.elem.html());
+
+			// set message - if set we use it, otherwise we try to pull from the _wModal_message container
+			var msg = this.modal.find('._wModal_msg');
+			if(msg.length)
+			{
+				if(this.settings.msg && this.settings.msg !== '') msg.html(this.settings.msg);
+				else this.settings.msg = msg.html();
+			}
+
+			$(window).resize(function(){ if(_this.modal.is(':visible')) _this.resetModal.apply(_this); });
+
+			this.resetBtns();
+
+			return this.modal;
+		},
+
+		resetModal: function()
+		{
+			var left = null, top = null, right = null, bottom = null;
+
+			var position = this.tempPosition || this.settings.position;
+			var offset = this.tempOffset || this.settings.offset;
+
+			var modalWidth = this.modal.outerWidth(true);
+			var modalHeight = this.modal.outerHeight(true);
+
+			var viewWidth = $(window).width();
+			var viewHeight = $(window).height();
+
+			this.outerX = viewWidth;
+			this.outerY = viewHeight;
+
+			var cX = (viewWidth/2) - (modalWidth/2);
+			var cY = (viewHeight/2) - (modalHeight/2);
+			var rX = viewWidth - modalWidth - offset;
+			var bY = viewHeight - modalHeight - offset;
+
+			switch(position)
+			{
+				case 'cm': left = cX;		top = cY;		break;
+				case 'lt': left = offset;	top = offset;	break;
+				case 'ct': left = cX;		top = offset; 	break;
+				case 'rt': left = rX;		top = offset;	break;
+				case 'rm': left = rX;		top = cY;		break;
+				case 'rb': left = rX;		top = bY;		break;
+				case 'cb': left = cX;		top = bY;		break;
+				case 'lb': left = offset;	top = bY;		break;
+				case 'lm': left = offset;	top = cY;		break;
+			}
+
+			this.modal.css({left:(left ? left + 'px' : 'auto'), top:(top ? top + 'px' : 'auto'), bottom:(bottom ? bottom + 'px' : 'auto'), right: (right ? right + 'px' : 'auto') });
+		},
+
+		resetBg: function()
+		{
+			this.bg.css({width:$(window).width(), height:$(window).height()});
+		},
+
+		resetBtns: function(btns)
+		{
+			var btns = btns || this.settings.btns;
+			var _this = this;
+
+			for(var btn in btns)
+			{
+				(function(btn){
+					_this.modal.find('._wModal_btn_' + btn).unbind('click');
+					_this.modal.find('._wModal_btn_' + btn).click(function()
+					{
+						if(_this.tempBtns[btn]) _this.tempBtns[btn].apply(_this);
+						else btns[btn].apply(_this);
+					});
+				})(btn);
+			}
+		},
+
+		show: function(settings)
+		{
+			this.tempBtns = settings.btns || {};
+			this.tempPosition = settings.position || null;
+			this.tempOffset = settings.offset || null;
+
+			this.tempFxShow = settings.fxShow || this.settings.fxShow;
+			this.tempFxHide = settings.fxHide || this.settings.fxHide;
+
+			var msg = this.modal.find('._wModal_msg');
+			if(msg.length) msg.html(settings.msg || this.settings.msg);
+
+			this.resetBg();
+			this.resetModal();
+
+			this.pixel.children('._wModal_holder').hide();
+
+			this['fxShow' + this.tempFxShow[0].toUpperCase() + this.tempFxShow.substring(1)].apply(this);
+		},
+
+		hide: function()
+		{
+			this['fxHide' + this.tempFxHide[0].toUpperCase() + this.tempFxHide.substring(1)].apply(this);
+		},
+
+		/************************************************
+		 * Effects
+		 ************************************************/
+
+		 /*** none ***/
+		 fxShowNone: function()
+		 {
+		 	this.bg.show();
+		 	this.modal.show();
+		 },
+
+		 fxHideNone: function()
+		 {
+		 	this.modal.hide();
+		 	this.bg.hide();
+		 },
+
+		 /*** fade ***/
+		 fxShowFade: function()
+		 {
+		 	var _this = this;
+		 	this.bg.fadeIn(100, function(){ _this.modal.fadeIn(300); });
+		 },
+
+		 fxHideFade: function()
+		 {
+		 	var _this = this;
+		 	this.modal.fadeOut(300, function(){ _this.bg.fadeOut(100); });
+		 },
+
+		 /*** slideUp ***/
+		 fxShowSlideUp: function(){ this.fxSlide('show', 'top', this.outerY); },
+		 fxHideSlideUp: function(){ this.fxSlide('hide', 'top', -1*this.outerY); },
+
+		 /*** slideDown ***/
+		 fxShowSlideDown: function(){ this.fxSlide('show', 'top', -1*this.outerY); },
+		 fxHideSlideDown: function(){ this.fxSlide('hide', 'top', this.outerY); },
+
+		 /*** slideLeft ***/
+		 fxShowSlideLeft: function(){ this.fxSlide('show', 'left', this.outerX); },
+		 fxHideSlideLeft: function(){ this.fxSlide('hide', 'left', -1*this.outerX); },
+
+		 /*** slideRight ***/
+		 fxShowSlideRight: function(){ this.fxSlide('show', 'left', -1*this.outerX); },
+		 fxHideSlideRight: function(){ this.fxSlide('hide', 'left', this.outerX); },
+
+		 /*** slide helper ***/
+		 fxSlide: function(state, position, value)
+		 {
+		 	var css = {};
+		 	css[position] = value;
+
+		 	if(state === 'show')
+		 	{
+			 	this.modal.show();
+			 	var offset = this.modal.offset();
+			 	this.modal.hide();
+
+			 	this.modal.css(css);
+			 	this.bg.show();
+			 	this.modal.show();
+			 	
+			 	css[position] = offset[position];
+			 	this.modal.animate(css);
+			 }
+			 else
+			 {
+			 	var _this = this;
+		 		this.modal.animate(css, function(){ _this.modal.hide(); _this.bg.hide(); });
+			 }
+		 }
+	}
+})(jQuery);
